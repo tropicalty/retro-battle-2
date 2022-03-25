fb7715c4f02bc28d8597c2364006fa1b37619896
tropicalty <90324965+tropicalty@users.noreply.github.com>
2022-03-25 08:00:00 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "hotdog":return img`
+. . . . f f f . . . f f f . . . 
+. . . f 2 2 2 f . f 2 2 2 f . . 
+. . f 2 1 1 2 2 f 2 2 2 2 2 f . 
+. . f 2 1 2 2 2 2 2 2 2 2 2 f . 
+. . f 2 2 2 2 2 2 2 2 2 2 2 f . 
+. . f 2 1 2 2 2 2 2 2 2 2 2 f . 
+. . . f 2 2 2 2 2 2 2 2 2 f . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . . f 2 2 2 2 2 f . . . . 
+. . . . . . f 2 2 2 f . . . . . 
+. . . . . . . f 2 f . . . . . . 
+. . . . . . . . f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
